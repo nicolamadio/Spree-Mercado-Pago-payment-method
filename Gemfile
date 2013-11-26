e7166ec7c308bc7258bcbe8cbd066a62247af21f
jmperez <jmperez@devartis.com>
2013-11-26 14:02:16 -0300

    Add spork for running tests.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,10 @@ source 'http://rubygems.org'
 gem 'spree', '~> 2.1.0'
 gem 'spree_auth_devise', :git => 'https://github.com/spree/spree_auth_devise.git', :branch => '2-1-stable'
 
+
+group :test do
+  gem "spork"
+  gem 'capybara'
+end
+
 gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'spork'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
@@ -33,32 +34,38 @@ require 'rubygems'
 
 
 # Configure Rails Environment
-ENV['RAILS_ENV'] = 'test'
-
-require File.expand_path('../dummy/config/environment.rb',  __FILE__)
-
-require 'rspec/rails'
-require 'ffaker'
-
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
-Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
-
-# Requires factories defined in spree_core
-require 'spree/testing_support/factories'
-require 'spree/testing_support/controller_requests'
-require 'spree/testing_support/authorization_helpers'
-require 'spree/testing_support/url_helpers'
 
 
-RSpec.configure do |config|
-  config.include FactoryGirl::Syntax::Methods
-  config.include Spree::TestingSupport::UrlHelpers
-  config.include Spree::TestingSupport::AuthorizationHelpers::Controller
+Spork.prefork do
+  ENV['RAILS_ENV'] = 'test'
 
-  config.mock_with :rspec
-  config.color = true
-  config.use_transactional_fixtures = false
+  require File.expand_path('../dummy/config/environment.rb',  __FILE__)
 
-  config.fail_fast = ENV['FAIL_FAST'] || false
+  require 'rspec/rails'
+  require 'ffaker'
+
+
+  # Requires supporting ruby files with custom matchers and macros, etc,
+  # in spec/support/ and its subdirectories.
+  Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+  # Requires factories defined in spree_core
+  require 'spree/testing_support/factories'
+  require 'spree/testing_support/controller_requests'
+  require 'spree/testing_support/authorization_helpers'
+  require 'spree/testing_support/url_helpers'
+
+
+  RSpec.configure do |config|
+    config.include FactoryGirl::Syntax::Methods
+    config.include Spree::TestingSupport::UrlHelpers
+    config.include Spree::TestingSupport::AuthorizationHelpers::Controller
+
+    config.mock_with :rspec
+    config.color = true
+    config.use_transactional_fixtures = false
+
+    config.fail_fast = ENV['FAIL_FAST'] || false
+  end
+
 end
